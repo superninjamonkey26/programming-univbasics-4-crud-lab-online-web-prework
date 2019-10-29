1ed840fdabb65a356bd6a9defff7ce355a2d8080
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-10-29 00:39:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["wow", "I", "am", "really"]
+  array = ["wow", "I", "am", "really" "learning"]
   element << "arrays!"
 end
 

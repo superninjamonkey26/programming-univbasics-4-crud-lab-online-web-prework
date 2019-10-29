d71f14f82c2ed42e940a7d4e78592a81121620c9
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-10-29 00:32:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array
-  ["Petey Panther", "Lily Butt", "Pancake", "Tinkerbelly"]
-  add_element_to_end_of_array << "Arya"
+  array = ["Petey Panther", "Lily Butt", "Pancake", "Tinkerbelly"]
+  element << "Arya"
 end
 
 def add_element_to_start_of_array(array, element)

6f56f207cc04b5796a56ab8ddb13415fdb545c59
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-10-29 00:59:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ end
 def add_element_to_start_of_array(array, element)
   array = ["I", "am", "really", "learning"]
   element = "wow"
-  add_element_to_start_of_array.unshift("wow")
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
